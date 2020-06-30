e681e6bd398f840322f2c200ee3d53258f719dbc
Ashish Babu <ashish.b@ellow.ai>
2020-06-30 12:05:31 +0530

    Modified folders with routes,controller and manager

--- backend_service/auth/src/email_api/email_api.manager.ts
+++ backend_service/auth/src/email_api/email_api.manager.ts
@@ -1,4 +1,5 @@
 import Query from './query/query';
+import nodemailer from "nodemailer"
 import database from '../common/database/database';
  export const addDetails = (email,otp) => {
      return new Promise((resolve, reject) => {
@@ -18,7 +19,30 @@ import database from '../common/database/database';
                     if (error) {
                         reject({ code: 400, message: "Failed. Please try again.", data: {} });
                         return;
-            })
+            });
+        
+    export const mailer = (email,otp) => {
+        return new Promise((resolve, reject) => {
+            var transporter = nodemailer.createTransport({
+                service: 'gmail',
+                auth: {
+                  user: 'yourmail@gmail.com',
+                  pass: 'password'
+                }
+              });
+              var mailOptions = {
+                from: 'yourmail@gmail.com',
+                to: email,
+                subject: 'Your Ellow.AI otp is here',
+                text:otp
+              };
+              transporter.sendMail(mailOptions, function(error, info){
+                if (error) {
+                  console.log(error);
+                } else {
+                  console.log('OTP has send successfully: ' + info.response);
+                }
+              });
+            }
         }
-    
 
