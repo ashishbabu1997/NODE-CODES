94e70cb58359e92dc9429143c84c1560e0d300ef
Ashish Babu <ashish.b@ellow.ai>
2020-07-07 16:26:15 +0530

    Added API Validator

---
+++ backend_service/company/companyProfile/company.manager.ts
@@ -0,0 +1,37 @@
+import companyQuery  from './query/company.query';
+import database from './common/database/database';
+
+export const get_Details = (_body) => {
+    return new Promise((resolve, reject) => {
+        const query = {
+            name: 'get_details',
+            text: companyQuery.get_details,
+            values: [_body.company_id],
+        }
+        database().query(query, (error, results) => {
+            if (error) {
+                console.log(query);
+                reject({ code: 400, message: "Failed. Please try again.", data: {} });
+                return;
+            }
+            resolve(results.rows[0].company_id);
+        })
+    });
+}
+export const update_Details = (_body) => {
+    return new Promise((resolve, reject) => {
+        const currentTime = Math.floor(Date.now() / 1000);
+        const query = {
+            name: 'update_details',
+            text: companyQuery.update_details,
+            values: [_body.company_name, _body.company_website, _body.company_size]
+        }
+        database().query(query, (error, results) => {
+            if (error) {
+                reject({ code: 400, message: "Failed. Please try again.", data: {} });
+                return;
+            }
+            resolve({ code: 200, message: "Location updated successfully", data: {} });
+        })
+    })
+}
---
+++ backend_service/company/companyProfile/company.router.ts
@@ -0,0 +1,9 @@
+import { getDetails,updateDetails } from './company.controller';
+import * as express from 'express';
+import validate from './middlewares/joiValidation';
+import updateDetailsSchema from './schemas/updateDetailsSchema';
+const router = express.Router();
+router
+    .get('/:compny_id',getDetails)
+    .put('/update_details', validate(updateDetailsSchema), updateDetails)
+export default router;
