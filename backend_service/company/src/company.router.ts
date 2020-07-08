8dd691432976d80e23a61c10bf75e4d9a1747089
Ashish Babu <ashish.b@ellow.ai>
2020-07-08 08:48:17 +0530

    Updated the company.router file and made changes

--- backend_service/company/src/company.router.ts
+++ backend_service/company/src/company.router.ts
@@ -1,10 +1,13 @@
 import AppConfig from './config/config';
 import locationRouter from './locations/locations.route';
+import companyProfileRouter from './companyProfile/companyProfile.router'
 import * as express from 'express';
 const router = express.Router();
 
 router
-    .use(`/api/${AppConfig.version}/company/locations`, locationRouter);
+    .use(`/api/${AppConfig.version}/company/locations`, locationRouter)
+    .use(`/api/${AppConfig.version}/company/companyProfile`,companyProfileRouter);
+
 
 export default router;
 
