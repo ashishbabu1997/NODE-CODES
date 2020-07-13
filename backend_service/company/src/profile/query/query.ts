2649f8aeb08b47e63a0c89bc079f2f57f00a72b7
Ashish Babu <ashish.b@ellow.ai>
2020-07-13 09:36:44 +0530

    Updated query

--- backend_service/company/src/profile/query/query.ts
+++ backend_service/company/src/profile/query/query.ts
@@ -1,4 +1,5 @@
 export default {
     get_details:"SELECT * FROM company WHERE id = ($1)",
-    update_details:"UPDATE company SET company_name=$1,company_website = $2,company_size=$3 WHERE company_id=$4"
+    update_details:"UPDATE company SET companyId=$1, profileUrl= $2,description=$3,logo=$4,coverPage=$5,tagline=$6,facebookId=$7,instagramId=$8,twitterId=$9,linkedinId=$10 WHERE companyId=$11"
 }
+
