c4e9fdac3e89183a4cede7f5d7145b0e6580822d
Joe Davis <joe.davis@cabotsolutions.com>
2020-07-16 16:41:30 +0530

    hiring steps api

--- backend_service/settings/src/hiringSteps/hiringSteps.route.ts
+++ backend_service/settings/src/hiringSteps/hiringSteps.route.ts
@@ -1,13 +1,15 @@
-import { createHiringSteps, getPositions } from './positions.controller';
+import { createHiringSteps, getHiringSteps, editHiringSteps } from './hiringStep.controller';
 import * as express from 'express';
 import validate from '../middlewares/joiVaildation';
-import addPositionSchema from './schemas/addPositionSchema';
+import addHiringStepSchema from './schemas/addHiringStepSchema';
+import editHiringStepSchema from './schemas/editHiringStepSchema';
 
 const router = express.Router();
 
 router
-    // .get('/', getPositions)
-    .post('/',createHiringSteps)
+    .get('/:companyId', getHiringSteps)
+    .post('/', validate(addHiringStepSchema), createHiringSteps)
+    .put('/', validate(editHiringStepSchema), editHiringSteps)
 
 export default router;
 
--- backend_service/settings/src/hiringSteps/query/hiringSteps.query.ts
+++ backend_service/settings/src/hiringSteps/query/hiringSteps.query.ts
@@ -1,10 +1,8 @@
 export default {
-    getCompanyPositionsASCSearch: `SELECT (SELECT COUNT(*) FROM positions WHERE status = true AND company_id =$1 AND position_name LIKE %$5%) as totalCount, position_name,location_name,created_on, job_status from positions WHERE status = true AND company_id = $1 AND position_name LIKE %$5% ORDER BY $2 ASC LIMIT $3 OFFSET $4`,
-    getCompanyPositionsASC: `SELECT (SELECT COUNT(*) FROM positions WHERE status = true AND company_id =$1 ) as totalCount, position_name,location_name,created_on, job_status from positions WHERE status = true AND company_id = $1 ORDER BY $2 ASC LIMIT $3 OFFSET $4`,
-    getCompanyPositionsDESCSearch: `SELECT (SELECT COUNT(*) FROM positions WHERE status = true AND company_id =$1 AND position_name LIKE $5) as totalCount, position_name,location_name,created_on, job_status from positions WHERE status = true AND company_id = $1 AND position_name LIKE $5 ORDER BY $2 DESC LIMIT $3 OFFSET $4`,
-    getCompanyPositionsDESC: `SELECT (SELECT COUNT(*) FROM positions WHERE status = true AND company_id =$1) as totalCount, position_name,location_name,created_on, job_status from positions WHERE status = true AND company_id = $1 ORDER BY $2 DESC LIMIT $3 OFFSET $4`,
-    addCompanyPositions: "INSERT into positions (position_name ,location_name ,developer_count ,company_id ,allow_remote ,experience_level ,job_description ,job_document ,contract_period ,currency_type_id ,billing_type ,min_budget ,max_budget ,hiring_step_id ,created_by ,updated_by ,created_on ,updated_on ) values ($1 ,$2 ,$3 ,$4 ,$5 ,$6,$7 ,$8 ,$9 ,$10 ,$11 ,$12 ,$13 ,$14 ,$15 ,$16 ,$17 ,$18 ) RETURNING position_id;",
-    addJobSkills: `INSERT into job_skills (position_id ,skill_id,created_on,updated_on ) values ($1, unnest ($2::int[]),$3,$4) ON CONFLICT ON CONSTRAINT position_skill DO NOTHING`,
-    addPositionSteps: `INSERT into position_hiring_steps (position_id,hiring_step_name,description,created_on,updated_on) values ($1,$2,$3,$4,$5) RETURNING position_hiring_step_id`,
-    addPositionHiringStages: `INSERT into position_hiring_stages (hiring_stage_name,description,position_hiring_step_id,hiring_stage_order,coordinator_id,created_on,updated_on) values `
+    getCompanyHiringSteps: "select hs.hiring_step_id ,hs.hiring_step_name ,hs.description ,hsg.hiring_stage_id ,hsg.hiring_stage_name ,hsg.hiring_stage_order ,hsg.description as hiring_stage_description from hiring_steps hs left join hiring_stages hsg on hsg.hiring_step_id = hs.hiring_step_id and hsg.status = true where hs.status = true and hs.company_id = $1",
+    addHiringSteps: `INSERT into hiring_steps (company_id,hiring_step_name,description,created_on,updated_on) values ($1,$2,$3,$4,$5) RETURNING hiring_step_id`,
+    addHiringStages: `INSERT into hiring_stages (hiring_stage_name,description,hiring_step_id,hiring_stage_order,created_on,updated_on) values `,
+    editHiringSteps: `UPDATE hiring_steps SET hiring_step_name=$2, description=$3,updated_on=$4 WHERE hiring_step_id=$1;`,
+    editHiringStagesStart: `update hiring_stages as hsg set hiring_stage_order = c.hiring_stage_order, hiring_stage_name = c.hiring_stage_name , description = c.description, updated_on = c.updated_on from ( values `,
+    editHiringStagesEnd:`) as c (hiring_stage_id,hiring_stage_name,description, hiring_stage_order,updated_on) where c.hiring_stage_id = hsg.hiring_stage_id`
 }
