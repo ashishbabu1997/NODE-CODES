2c2ff0e252bfeb9f80c886e9ebb3259fd53cd92d
seeth <seethal.raj@ellow.ai>
2020-07-17 04:10:25 -0700

    user_id -  not-null constraint issue fixed

--- backend_service/employee/src/query/employee.query.ts
+++ backend_service/employee/src/query/employee.query.ts
@@ -1,4 +1,4 @@
 export default {
-    "createCompany": "INSERT INTO company( company_name, company_website, company_size_id, created_on) VALUES ($1, $2, $3, $4) RETURNING company_id",
+    "createCompany": "INSERT INTO company( company_name, company_website, company_size_id, created_on, user_id, company_type, company_type_id) VALUES ($1, $2, $3, $4, 1, 1, 1) RETURNING company_id",
     "createEmployee":"INSERT INTO employee(firstname, lastname, account_type, company_id, telephone_number, role_id, created_on, status) VALUES ($1, $2, $3, $4, $5, $6, $7, true)"
 }
