2b6ea5df126667c7676d088088d6afcbfc5ef630
seeth <seethal.raj@ellow.ai>
2020-07-26 22:13:22 -0700

    Certificate table CRUD

--- backend_service/settings/src/settings.router.ts
+++ backend_service/settings/src/settings.router.ts
@@ -1,12 +1,14 @@
 import AppConfig from './config/config';
 import hiringStepsRouter from './hiringSteps/hiringSteps.route';
 import preferencesRouter from './preferences/preferences.route';
+import certificateRouter from './certificate/certificate.route';
 import * as express from 'express';
 const router = express.Router();
 
 router
     .use(`/api/${AppConfig.version}/settings/hiringSteps`, hiringStepsRouter)
     .use(`/api/${AppConfig.version}/settings/preferences`, preferencesRouter)
+    .use(`/api/${AppConfig.version}/settings/certificate`, certificateRouter)
 
 export default router;
 
