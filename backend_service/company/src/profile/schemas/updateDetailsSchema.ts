6acbfebbb9f6428e7571593d25ee1e2f3b70172c
Ashish Babu <ashish.b@ellow.ai>
2020-07-27 23:23:41 +0530

    Updated Profile API

--- backend_service/company/src/profile/schemas/updateDetailsSchema.ts
+++ backend_service/company/src/profile/schemas/updateDetailsSchema.ts
@@ -1,162 +1,162 @@
 import * as Joi from '@hapi/joi';
 
 export default Joi.object().keys({
-    companyId: Joi.number().required().error(errors => {
-        errors.forEach(err => {
-            switch (err.code) {
-                case "any.required":
-                    err.message = "Company Id should not be empty";
-                    break;
-                case "number.base":
-                    err.message = "Company Id must be a number"
-                    break;
-                default:
-                    err.message = "Invalid Company Id"
-                    break;
-            }
-        });
-        return errors;
-    }),
-    profileUrl: Joi.string().required().error(errors => {
-        errors.forEach(err => {
-            console.log(err)
+    profileUrl: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
+            console.log(err);
             switch (err.code) {
                 case "any.required":
                     err.message = "Profile URL should not be empty!";
                     break;
                 case "string.base":
-                    err.message = "Profile URL must be a string"
+                    err.message = "Profile URL must be a string";
                     break;
                 default:
-                    err.message = "Invalid Profile URL"
+                    err.message = "Invalid Profile URL";
                     break;
             }
         });
         return errors;
     }),
-    description: Joi.string().required().error(errors => {
-        errors.forEach(err => {
+    description: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
             switch (err.code) {
                 case "any.required":
                     err.message = "Description  should not be empty!";
                     break;
                 case "string.base":
-                    err.message = "Description should be string"
+                    err.message = "Description should be string";
                     break;
                 default:
-                    err.message = "Description is invalid"
+                    err.message = "Description is invalid";
                     break;
             }
         });
         return errors;
     }),
-    logo: Joi.string().required().error(errors => {
-        errors.forEach(err => {
+    logo: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
             switch (err.code) {
                 case "any.required":
                     err.message = "logo  should not be empty!";
                     break;
                 case "string.base":
-                    err.message = "logo should be string"
+                    err.message = "logo should be string";
                     break;
                 default:
-                    err.message = "logo is invalid"
+                    err.message = "logo is invalid";
                     break;
             }
         });
         return errors;
     }),
-    coverPage: Joi.string().required().error(errors => {
-        errors.forEach(err => {
+    coverPage: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
             switch (err.code) {
                 case "any.required":
                     err.message = "Cover Page  should not be empty!";
                     break;
                 case "string.base":
-                    err.message = "Cover Page should be string"
+                    err.message = "Cover Page should be string";
                     break;
                 default:
-                    err.message = "Cover Page is invalid"
+                    err.message = "Cover Page is invalid";
                     break;
             }
         });
         return errors;
     }),
-    tagline: Joi.string().required().error(errors => {
-        errors.forEach(err => {
+    tagline: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
             switch (err.code) {
                 case "any.required":
                     err.message = "Tagline  should not be empty!";
                     break;
                 case "string.base":
-                    err.message = "Tagline should be string"
+                    err.message = "Tagline should be string";
                     break;
                 default:
-                    err.message = "Tagline is invalid"
+                    err.message = "Tagline is invalid";
                     break;
             }
         });
         return errors;
     }),
-    facebookId: Joi.number().required().error(errors => {
-        errors.forEach(err => {
+    facebookId: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
             switch (err.code) {
                 case "any.required":
                     err.message = "Facebook Id should not be empty";
                     break;
-                case "number.base":
-                    err.message = "Facebook Id must be a number"
+                case "string.base":
+                    err.message = "Facebook Id must be a string of charectors";
                     break;
                 default:
-                    err.message = "Invalid Facebook Id"
+                    err.message = "Invalid Facebook Id";
                     break;
             }
         });
         return errors;
     }),
-    instagramId: Joi.number().required().error(errors => {
-        errors.forEach(err => {
+    instagramId: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
             switch (err.code) {
                 case "any.required":
                     err.message = "Instagram Id should not be empty";
                     break;
-                case "number.base":
-                    err.message = "Instagram Id must be a number"
+                case "string.base":
+                    err.message = "Instagram Id must be a string of charectors";
                     break;
                 default:
-                    err.message = "Invalid Instagram Id"
+                    err.message = "Invalid Instagram Id";
                     break;
             }
         });
         return errors;
     }),
-    twitterId: Joi.number().required().error(errors => {
-        errors.forEach(err => {
+    twitterId: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
             switch (err.code) {
                 case "any.required":
                     err.message = "Twitter Id should not be empty";
                     break;
-                case "number.base":
-                    err.message = "Twitter Id must be a number"
+                case "string.base":
+                    err.message = "Twitter Id must be a string of charactors";
                     break;
                 default:
-                    err.message = "Invalid Twitter Id"
+                    err.message = "Invalid Twitter Id";
                     break;
             }
         });
         return errors;
     }),
-    linkedinId: Joi.number().required().error(errors => {
-        errors.forEach(err => {
+    linkedinId: Joi.string().required().error(function (errors) {
+        errors.forEach(function (err) {
             switch (err.code) {
                 case "any.required":
                     err.message = "Linkedin Id should not be empty";
                     break;
-                case "number.base":
-                    err.message = "Linkedin Id must be a number"
+                case "string.base":
+                    err.message = "Linkedin Id must be a string of charectors";
                     break;
                 default:
-                    err.message = "Invalid Linkedin Id"
+                    err.message = "Invalid Linkedin Id";
+                    break;
+            }
+        });
+        return errors;
+    }),
+    companyId: Joi.number().required().error(function (errors) {
+        errors.forEach(function (err) {
+            switch (err.code) {
+                case "any.required":
+                    err.message = "Company Id should not be empty";
+                    break;
+                case "number.base":
+                    err.message = "Company Id must be a number";
+                    break;
+                default:
+                    err.message = "Invalid Company Id";
                     break;
             }
         });
