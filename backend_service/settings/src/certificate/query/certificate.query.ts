d305563c537b97b6d14cbce7321de09913b8ea97
seeth <seethal.raj@ellow.ai>
2020-07-27 19:45:35 -0700

    column alias casing corrected

--- backend_service/settings/src/certificate/query/certificate.query.ts
+++ backend_service/settings/src/certificate/query/certificate.query.ts
@@ -1,5 +1,5 @@
 export default {
-    getCertificate : "select certificate_id as certificateId, certificate_name  as certificateName from certificates c where status = true",
+    getCertificate : 'select certificate_id as "certificateId", certificate_name  as "certificateName" from certificates c where status = true',
     addCertificate : "insert into certificates(certificate_name, created_on, status) values ($1, $2, true)",
     updateCertificate : "update certificates set certificate_name = $1, updated_on = $2 where certificate_id = $3",
     deleteCertificate : "update certificates set status = false where certificate_id = $1"
