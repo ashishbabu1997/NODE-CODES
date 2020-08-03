57ebf62b04ab9d95aa04db7063a528b6676c2072
Ashish Babu <ashish.b@ellow.ai>
2020-08-03 15:45:46 +0530

    updated

--- backend_service/auth/src/config/config.ts
+++ backend_service/auth/src/config/config.ts
@@ -13,8 +13,8 @@ export default {
     },
     "mail": {
         "service": "gmail",
-        "user": "ashishpallikkunnel97@gmail.com",
-        "password": "ash1526$",
+        "user": "ashish.babu@ellow.ai",
+        "password": "Ash1526$",
     },
     "jwtSecretKey": "elL0wA!"
 }
