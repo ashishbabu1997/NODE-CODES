776904ffa694f4f5706b39189ada0d5e221cdea8
Joe Davis <joe.davis@cabotsolutions.com>
2020-08-07 14:22:47 +0530

    bug fix

--- backend_service/settings/src/roles/query/ServicesQuery.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    getServices: `select service_id as "serviceId", service_name as "serviceName" from  services where status = true`,
-    createServices: "INSERT into services (service_name,created_on ,updated_on ) values ($1 ,$2,$2 );",
-    updateServices: `update services set service_name = $1, updated_on = $2 where service_id = $3 and status = true`,
-    deleteServices: `update services set status = false,updated_on = $1 where service_id = $2`
-}
