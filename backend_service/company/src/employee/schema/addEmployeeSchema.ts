6adcf28e53fe5abc6a513e640ecf7fd6d40b94de
Ashish Babu <ashish.b@ellow.ai>
2020-08-21 15:06:03 +0530

    Updated addEmployeeSchema

--- backend_service/company/src/employee/schema/addEmployeeSchema.ts
+++ backend_service/company/src/employee/schema/addEmployeeSchema.ts
@@ -97,7 +97,7 @@ export default Joi.object().keys({
         });
         return errors;
     }),
-    document: Joi.string().error(errors => {
+    document: Joi.string().allow('').error(errors => {
         errors.forEach(err => {
             switch (err.code) {
                 case "string.base":
