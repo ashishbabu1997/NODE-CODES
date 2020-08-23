462b95fa3e680101b0084c1113335ac3b6cc9d9f
Ashish Babu <ashish.b@ellow.ai>
2020-08-23 18:59:19 +0530

    Removed exrtra array from countries

--- backend_service/settings/src/states/statesApi.controller.ts
+++ backend_service/settings/src/states/statesApi.controller.ts
@@ -2,8 +2,8 @@ import {listStates} from './statesApi.manager';
 import sendResponse from '../common/response/response';
 
 export const getStatesList = (req, res) => {
-    const body = req.body;
-    listStates(body).then((response: any) => {
+    // const body = req.body;
+    listStates().then((response: any) => {
         sendResponse(res, response.code, 1,201, response.message, response.data)
     }).catch(error => {
         sendResponse(res, error.code, 0,401, error.message, error.data)
--- backend_service/settings/src/states/statesApi.manager.ts
+++ backend_service/settings/src/states/statesApi.manager.ts
@@ -1,8 +1,8 @@
 import config from '../config/config'
-export const listStates = (_body) => {
+export const listStates = ()=> {
     return new Promise((resolve, reject) => {
-            const states=config.states
-            const result = states.filter(state => state.countryId =_body.countryId);
-            resolve({ code: 200, message: "Countries listed Succesfully", data: { states: result } });
+            // const states=config.states
+            // const result = states.filter(state => state.countryId =_body.countryId);
+            resolve({ code: 200, message: "Countries listed Succesfully", data: { states: config.states } });
     });
 }
