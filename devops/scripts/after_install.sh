f022577ceacb6e35a81c3faa508efae9ac9ff276
adarsh_ts <adarsh.ts@ADRAPIDVALUE.COM>
2020-08-25 00:05:21 +0530

    node pipeline script updates

--- devops/scripts/after_install.sh
+++ devops/scripts/after_install.sh
@@ -2,6 +2,26 @@
 pwd
 
 NODE_PATH=/DEV/devops/BE/codedeploy
+cd $NODE_PATH/backend_service/auth
+npm insatll
+sleep 30                   
+cd $NODE_PATH/backend_service/company 
+npm insatll
+sleep 30                   
+cd $NODE_PATH/backend_service/employee
+npm insatll                   
+sleep 30
+cd $NODE_PATH/backend_service/jobs
+npm insatll                   
+sleep 30
+cd $NODE_PATH/backend_service/settings
+npm insatll                   
+sleep 30
+cd $NODE_PATH/backend_service/candidate
+npm insatll
+sleep 30
+
+NODE_PATH=/DEV/devops/BE/codedeploy
 cd $NODE_PATH/backend_service/auth && ls -l
 cd $NODE_PATH/backend_service/company && ls -l
 cd $NODE_PATH/backend_service/employee && ls -l
