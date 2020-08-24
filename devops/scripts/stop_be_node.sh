9c64a0acb583f7d13619c37ca409b4fefe3804eb
adarsh_ts <adarsh.ts@ADRAPIDVALUE.COM>
2020-08-24 10:44:41 +0530

    node pipeline script updates

--- devops/scripts/stop_be_node.sh
+++ devops/scripts/stop_be_node.sh
@@ -2,9 +2,12 @@
 NODE_PATH=/DEV/devops/BE/codedeploy
 ls -l $NODE_PATH
 pwd
+echo "netstat -nlpt |grep :400[0-5]"
 netstat -nlpt |grep :400[0-5]
 # stop BE node services running on 4000 , 4001, 4002, 4003, 4004 and 40005
+echo "/usr/local/bin/pm2 stop default"
 /usr/local/bin/pm2 stop default
 echo "netstat -nlpt |grep :400[0-5]"
 netstat -nlpt |grep :400[0-5]
 rm -rf $NODE_PATH
+	
