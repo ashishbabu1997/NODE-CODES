8ef285bd3e511346a3981f06b1e98204f24b1432
adarsh_ts <adarsh.ts@ADRAPIDVALUE.COM>
2020-08-24 23:42:25 +0530

    pipeline changes added

--- devops/scripts/install_nodemodules.sh
+++ devops/scripts/install_nodemodules.sh
@@ -2,9 +2,21 @@
 pwd
 
 NODE_PATH=/DEV/devops/BE/codedeploy
-cd $NODE_PATH/backend_service/auth && npm insatll                   
-cd $NODE_PATH/backend_service/company && npm insatll                   
-cd $NODE_PATH/backend_service/employee && npm insatll                   
-cd $NODE_PATH/backend_service/jobs && npm insatll                   
-cd $NODE_PATH/backend_service/settings && npm insatll                   
-cd $NODE_PATH/backend_service/candidate && npm insatll
+cd $NODE_PATH/backend_service/auth
+npm insatll
+sleep 30                   
+cd $NODE_PATH/backend_service/company 
+npm insatll
+sleep 30                   
+cd $NODE_PATH/backend_service/employee
+npm insatll                   
+sleep 30
+cd $NODE_PATH/backend_service/jobs
+npm insatll                   
+sleep 30
+cd $NODE_PATH/backend_service/settings
+npm insatll                   
+sleep 30
+cd $NODE_PATH/backend_service/candidate
+npm insatll
+sleep 30
--- devops/scripts/start_node.sh
+++ devops/scripts/start_node.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 pwd
 NODE_PATH=/DEV/devops/BE/codedeploy
-cd $NODE_PATH/backend_service/auth && npm run prod && npm run prod && npm run prod && sleep 10 
-cd $NODE_PATH/backend_service/company && npm run prod && npm run prod && npm run prod && sleep 10 
-cd $NODE_PATH/backend_service/employee && npm run prod && npm run prod && npm run prod && sleep 10 
-cd $NODE_PATH/backend_service/jobs && npm run prod && npm run prod && npm run prod && sleep 10 
-cd $NODE_PATH/backend_service/settings && npm run prod && npm run prod && npm run prod && sleep 10 
-cd $NODE_PATH/backend_service/candidate && npm run prod && npm run prod && npm run prod && sleep 10 
+cd $NODE_PATH/backend_service/auth 
+npm run prod
+npm run prod
+npm run prod
+ sleep 10 
+cd $NODE_PATH/backend_service/company 
+npm run prod
+npm run prod
+npm run prod
+ sleep 10 
+cd $NODE_PATH/backend_service/employee 
+npm run prod
+npm run prod
+npm run prod
+ sleep 10 
+cd $NODE_PATH/backend_service/jobs 
+npm run prod
+npm run prod
+npm run prod
+ sleep 10  
+cd $NODE_PATH/backend_service/settings
+npm run prod
+npm run prod
+npm run prod
+ sleep 10 
+cd $NODE_PATH/backend_service/candidate
+npm run prod
+npm run prod
+npm run prod
+ sleep 10 
 netstat -nlpt |grep :400[0-5]
