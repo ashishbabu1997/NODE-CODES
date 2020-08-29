abe2ed041db7d46a68515b13a218ade9dff3c1a0
Ashish Babu <ashish.b@ellow.ai>
2020-08-29 19:05:59 +0530

    Checked for registered email in forgot password API

--- backend_service/auth/src/forgotPassword/forgotPassword.manager.ts
+++ backend_service/auth/src/forgotPassword/forgotPassword.manager.ts
@@ -6,23 +6,35 @@ import Query from './query/query';
 import database from '../common/database/database';
 export const sendLink = (_body) => {
   return new Promise((resolve, reject) => {
-    var payload = { email:_body.email}
-    var secret = jwtConfig.jwtSecretKey
-    var token = jwt.encode(payload, secret);
-    const insertTokenQuery = {
-      name: 'insertToken',
-      text: Query.insertToken,
-      values: [_body.email,token]
+    const checkEMail = {
+      name: 'check-email',
+      text: Query.checkEmail,
+      values: [_body.email]
     }
-    database().query(insertTokenQuery, (error, results) => {
+    database().query(checkEMail, (error, results) => {
       if (error) {
           reject({ code: 400, message: "Database connection Error !!!!", data:  {} });
           return;
         }
-      var link="http://localhost:3000/passwordset/"+token
-      const subject="ellow.ai RESET PASSWORD LINK"
-      const text="Click on the link to reset your password : "
-      sendMail(_body.email, subject,text+link, function(err,data) {
+      else if(results.rowCount==1)
+      {
+        var payload = { email:_body.email}
+        var secret = jwtConfig.jwtSecretKey
+        var token = jwt.encode(payload, secret);
+        const insertTokenQuery = {
+          name: 'insertToken',
+          text: Query.insertToken,
+          values: [_body.email,token]
+        }
+        database().query(insertTokenQuery, (error, results) => {
+        if (error) {
+          reject({ code: 400, message: "Database connection Error !!!!", data:  {} });
+          return;
+        }
+        var link="http://localhost:3000/passwordset/"+token
+        const subject="ellow.ai RESET PASSWORD LINK"
+        const text="Click on the link to reset your password : "
+        sendMail(_body.email, subject,text+link, function(err,data) {
                 if (err) {
                   console.log("........Email ERROR:.........",err)
                   reject({ code: 400, message: "Cannot send email", data:{}});
@@ -32,5 +44,14 @@ export const sendLink = (_body) => {
                 resolve({ code: 200, message: "Token  has sent to your email successfully", data:{} });
             });
           })
-        })
+      }
+      else
+      {
+        reject({ code: 400, message: "Email does'nt Exist", data:{}});
+        return;
+
+      }
+    })
+
+  })
 }
--- backend_service/auth/src/forgotPassword/query/query.ts
+++ backend_service/auth/src/forgotPassword/query/query.ts
@@ -1,3 +1,4 @@
 export default {
-    insertToken:`UPDATE employee SET token=$2 WHERE email=$1 `
+    insertToken:`UPDATE employee SET token=$2 WHERE email=$1 `,
+    checkEmail:'SELECT password from employee WHERE email=$1'
 };
