ce905a6adaa89acbb9a8a25d66fac74abb0474a5
Ashish Babu <ashish.b@ellow.ai>
2020-09-09 22:15:12 +0530

    Added status code to customer signup

--- backend_service/employee/src/employee.controller.ts
+++ backend_service/employee/src/employee.controller.ts
@@ -1,11 +1,12 @@
 import {createEmployee } from './employee.manager';
 import sendResponse from './common/response/response';
+import response from './common/response/response';
 
   export const addEmployee = (req, res) => {
     const body = req.body;
     createEmployee(body).then((response: any) => {
         sendResponse(res, response.code, 1,201, response.message, response.data)
     }).catch(error => {
-        sendResponse(res, error.code, 0,401, error.message, error.data)
+        sendResponse(res, error.code, 0,error.statusCode, error.message, error.data)
     })
 }
