3d351257f858bc8f9456a801be29857264eef5e1
Ashish Babu <ashish.b@ellow.ai>
2020-09-10 20:26:01 +0530

    Changes in countries API

--- backend_service/candidate/src/candidates/candidates.manager.ts
+++ backend_service/candidate/src/candidates/candidates.manager.ts
@@ -95,87 +95,87 @@ export const listCandidatesDetails = (_body) => {
         })
     })
 }
-export const candidateClearance = (_body) => {
-    return new Promise((resolve, reject) => {
-        if (_body.decisionValue==1)
-        {
-            if(_body.userRoleId==1)
-            {
-                var adminApproveStatus=1
-                var adminComments=_body.adminComment
+// export const candidateClearance = (_body) => {
+//     return new Promise((resolve, reject) => {
+//         if (_body.decisionValue==1)
+//         {
+//             if(_body.userRoleId==1)
+//             {
+//                 var adminApproveStatus=1
+//                 var adminComments=_body.adminComment
 
-            }
-            const candidateApprovalQuery = {
-                name: 'admin-panel',
-                text:candidateQuery.candidateClearanceQuery,
-                values:[_body.employeeId,true,1]
-            }
-            database().query(adminApprovalQuery, (error, results) => {
-                if (error) {
-                    reject({ code: 400, message: "Database Error", data: {} });
-                    return;
-                }
-                // resolve({ code: 200, message: "Users listed successfully", data: { Users: results.rows } });
-            })
-            const password = passwordGenerator.generate({
-                length: 10,
-                numbers: true
-            });
-            var hashedPassword = crypto.createHash("sha256").update(password).digest("hex");
-            const subject = " ellow.ai LOGIN PASSWORD "
-            const storePasswordQuery = {
-                name: 'store-encrypted-password',
-                text: admineQuery.storePassword,
-                values: [hashedPassword,_body.email],
-            }
-            database().query(storePasswordQuery, (error, results) => {
-                if (error) {
-                    console.log(error)
-                    reject({ code: 400, message: "Database Error", data: {} });
-                    return;
-                }
-            })
-            // var desc=_body.description
-            // var description=desc.fontsize(3).bold()
-            var textFormat = config.text.firstLine + config.nextLine + config.text.secondLine + config.nextLine+config.text.thirdLine + config.nextLine + config.text.password + password + config.nextLine + config.text.fourthLine + config.nextLine + config.text.fifthLine
-            sendMail(_body.email, subject, textFormat, function (err, data) {
-                if (err) {
-                    console.log(err)
-                    reject({ code: 400, message: "Database Error", data: {} });
-                    return;
-                }
-                console.log('A password has been send to your email !!!');
-                resolve({ code: 200, message: "User Approval Successfull", data: {} });
-            });
-        }
-        else
-        {
-            const adminApprovalQuery = {
-                name: 'admin-panel',
-                text:admineQuery.clearanceQuery,
-                values:[_body.employeeId,false,0]
-            }
-            database().query(adminApprovalQuery, (error, results) => {
-                if (error) {
-                    console.log(error)
-                    reject({ code: 400, message: "Database Error", data: {} });
-                    return;
-                }
-            })
-            var desc=_body.description
-            var description=desc.fontsize(3).bold()
-            var subject="ellow.ai ACCOUNT REJECTION MAIL "
-            var textFormat = config.rejectText.firstLine + config.nextLine + config.rejectText.secondLine + config.nextLine+description+config.nextLine+config.rejectText.thirdLine + config.nextLine + config.rejectText.fourthLine + config.nextLine + config.rejectText.fifthLine
-            sendMail(_body.email, subject, textFormat, function (err, data) {
-                if (err) {
-                    console.log(err)
-                    return;
-                }
-                console.log('An admin rejection message has been sent to your email!!!');
-                resolve({ code: 200, message: "User Rejection Successfull", data: {} });
+//             }
+//             const candidateApprovalQuery = {
+//                 name: 'admin-panel',
+//                 text:candidateQuery.candidateClearanceQuery,
+//                 values:[_body.employeeId,true,1]
+//             }
+//             database().query(adminApprovalQuery, (error, results) => {
+//                 if (error) {
+//                     reject({ code: 400, message: "Database Error", data: {} });
+//                     return;
+//                 }
+//                 // resolve({ code: 200, message: "Users listed successfully", data: { Users: results.rows } });
+//             })
+//             const password = passwordGenerator.generate({
+//                 length: 10,
+//                 numbers: true
+//             });
+//             var hashedPassword = crypto.createHash("sha256").update(password).digest("hex");
+//             const subject = " ellow.ai LOGIN PASSWORD "
+//             const storePasswordQuery = {
+//                 name: 'store-encrypted-password',
+//                 text: admineQuery.storePassword,
+//                 values: [hashedPassword,_body.email],
+//             }
+//             database().query(storePasswordQuery, (error, results) => {
+//                 if (error) {
+//                     console.log(error)
+//                     reject({ code: 400, message: "Database Error", data: {} });
+//                     return;
+//                 }
+//             })
+//             // var desc=_body.description
+//             // var description=desc.fontsize(3).bold()
+//             var textFormat = config.text.firstLine + config.nextLine + config.text.secondLine + config.nextLine+config.text.thirdLine + config.nextLine + config.text.password + password + config.nextLine + config.text.fourthLine + config.nextLine + config.text.fifthLine
+//             sendMail(_body.email, subject, textFormat, function (err, data) {
+//                 if (err) {
+//                     console.log(err)
+//                     reject({ code: 400, message: "Database Error", data: {} });
+//                     return;
+//                 }
+//                 console.log('A password has been send to your email !!!');
+//                 resolve({ code: 200, message: "User Approval Successfull", data: {} });
+//             });
+//         }
+//         else
+//         {
+//             const adminApprovalQuery = {
+//                 name: 'admin-panel',
+//                 text:admineQuery.clearanceQuery,
+//                 values:[_body.employeeId,false,0]
+//             }
+//             database().query(adminApprovalQuery, (error, results) => {
+//                 if (error) {
+//                     console.log(error)
+//                     reject({ code: 400, message: "Database Error", data: {} });
+//                     return;
+//                 }
+//             })
+//             var desc=_body.description
+//             var description=desc.fontsize(3).bold()
+//             var subject="ellow.ai ACCOUNT REJECTION MAIL "
+//             var textFormat = config.rejectText.firstLine + config.nextLine + config.rejectText.secondLine + config.nextLine+description+config.nextLine+config.rejectText.thirdLine + config.nextLine + config.rejectText.fourthLine + config.nextLine + config.rejectText.fifthLine
+//             sendMail(_body.email, subject, textFormat, function (err, data) {
+//                 if (err) {
+//                     console.log(err)
+//                     return;
+//                 }
+//                 console.log('An admin rejection message has been sent to your email!!!');
+//                 resolve({ code: 200, message: "User Rejection Successfull", data: {} });
 
-            });
+//             });
 
-        }       
-})
-}
+//         }       
+// })
+// }
