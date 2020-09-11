adc292954571eba4d50fc870a32ca74534484a27
Ashish Babu <ashish.b@ellow.ai>
2020-09-11 13:44:32 +0530

    Resolved error in jobreceived

--- backend_service/settings/src/skills/query/SkillsQuery.ts
+++ backend_service/settings/src/skills/query/SkillsQuery.ts
@@ -1,5 +1,5 @@
 export default {
-    getSkills: `select s.skill_id as "skillId", s.skill_name as "skillName" from  skills s left join job_category_skills jcs on jcs .skill_id = s.skill_id and jcs.status = true where jcs.job_category_id = $1 and s.status  = true`,
+    getSkills: `select s.skill_id as "skillId", s.skill_name as "skillName",s.top_rated as "topRated" from  skills s left join job_category_skills jcs on jcs .skill_id = s.skill_id and jcs.status = true where jcs.job_category_id = $1 and s.status  = true`,
     createSkills: "INSERT into skills (skill_name,created_on ,updated_on ) values ($1,$2,$2 ) ON CONFLICT ON CONSTRAINT skills_skill_name_status_key DO UPDATE SET skill_name = $1 RETURNING skill_id ;",
     updateSkills: `update skills set skill_name = $1, job_category_id = $4, updated_on = $2 where skill_id = $3 and status = true`,
     deleteSkills: `update skills set status = false,updated_on = $1 where skill_id = $2`,
