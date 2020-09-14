1072cfc1121845e85422d0e46289dee0aed8f09f
Joe Davis <joe.davis@cabotsolutions.com>
2020-09-14 22:59:46 +0530

    fixes

--- backend_service/candidate/src/candidates/candidates.manager.ts
+++ backend_service/candidate/src/candidates/candidates.manager.ts
@@ -20,7 +20,7 @@ export const getCandidateDetails = (_body) => {
             let result = {
                 candidateFirstName: candidate[0].candidateFirstName,
                 candidateLastName: candidate[0].candidateLastName,
-                companyName:candidate[0].companyName,
+                companyName: candidate[0].companyName,
                 positionName: candidate[0].positionName,
                 description: candidate[0].description,
                 coverNote: candidate[0].coverNote,
@@ -48,15 +48,17 @@ export const getCandidateDetails = (_body) => {
 export const listCandidatesDetails = (_body) => {
     return new Promise((resolve, reject) => {
         var selectQuery = candidateQuery.listCandidates;
-        if (_body.filter) {
-            selectQuery = selectQuery + " " + "AND ((LOWER(ca.candidate_first_name) LIKE '%" + _body.filter.toLowerCase() + "%') " + "OR ((LOWER(ca.candidate_last_name) LIKE '%" + _body.filter.toLowerCase() + "%') " + "OR (LOWER(c.company_name) LIKE '%" + _body.filter.toLowerCase() + "%')) "
+        if (_body.userRoleId != 1) {
+            selectQuery = selectQuery + " AND ca.admin_approve_status = 1"
         }
-
+        if (_body.filter) {
+            selectQuery = selectQuery + " " + "AND ((LOWER(ca.candidate_first_name) LIKE '%" + _body.filter.toLowerCase() + "%') " + "OR (LOWER(ca.candidate_last_name) LIKE '%" + _body.filter.toLowerCase() + "%') " + "OR (LOWER(c.company_name) LIKE '%" + _body.filter.toLowerCase() + "%')) "
+        }
         (async () => {
             const client = await database().connect()
             try {
                 await client.query('BEGIN');
-                const [adminHiringStages, adminApprovedCandidates] = _body.userRoleId == 1 ? [1, 0] : [0, 1];
+                const adminHiringStages = _body.userRoleId == 1 ? 1 : 0;
                 const listHiringStages = {
                     name: 'get-position-hiring-stages',
                     text: candidateQuery.getPositionHiringStages,
@@ -64,14 +66,18 @@ export const listCandidatesDetails = (_body) => {
                 }
                 const hiringStagesResult = await client.query(listHiringStages);
                 let hiringStages = hiringStagesResult.rows;
+
+                const { companyName, positionName } = hiringStages[0];
                 hiringStages = hiringStages.map(element => { return { ...element, candidateList: [] } })
                 const listCandidates = {
                     name: 'get-position-candidates',
                     text: selectQuery,
-                    values: [_body.positionId, adminApprovedCandidates]
+                    values: [_body.positionId]
                 }
                 const candidatesResult = await client.query(listCandidates);
-                let candidates = candidatesResult.rows
+                let candidates = candidatesResult.rows;
+
+                console.log(companyName, "dasdas")
                 let allCandidates = [];
                 candidates.forEach(candidate => {
                     let index = hiringStages.findIndex(e => e.positionHiringStageId == candidate.currentHiringStageId)
@@ -84,7 +90,7 @@ export const listCandidatesDetails = (_body) => {
                     }
                 });
                 await client.query('COMMIT')
-                resolve({ code: 200, message: "Candidate Listed successfully", data: { hiringStages, allCandidates } });
+                resolve({ code: 200, message: "Candidate Listed successfully", data: { companyName, positionName, hiringStages, allCandidates } });
             } catch (e) {
                 console.log(e)
                 await client.query('ROLLBACK')
@@ -110,8 +116,8 @@ export const candidateClearance = (_body) => {
         var value;
         const getCandidateName = {
             name: 'get-candidate-names',
-            text:candidateQuery.getCandidateNames,
-            values:[_body.candidateId]
+            text: candidateQuery.getCandidateNames,
+            values: [_body.candidateId]
         }
         database().query(getCandidateName, (error, results) => {
             if (error) {
@@ -119,85 +125,79 @@ export const candidateClearance = (_body) => {
                 reject({ code: 400, message: "Database Error", data: {} });
                 return;
             }
-        var firstName=results.rows[0].firstName
-        var companyName=results.rows[0].companyName
-        candidateFirstName=firstName.fontsize(3).bold()
-        candidateCompanyName=companyName.fontsize(3).bold()    
-        if (_body.decisionValue==1)
-        {
-            makeOffer=1
-            if(_body.userRoleId==1)
-            {
-                adminApproveStatus=1
-                comment=_body.comment
-                value=[_body.candidateId,adminApproveStatus,comment,_body.elowRate,makeOffer]
-                candidateQueries=candidateQuery.candidateSuperAdminApprovalQuery
-            }
-            else if(_body.userRoleId==2)
-            {
-                adminApproveStatus=1;
-                comment=_body.comment;
-                value=[_body.candidateId,adminApproveStatus,comment,makeOffer]
-                candidateQueries=candidateQuery.candidateAdminApprovalQuery
-                subj="Candidate Approval Mail";
-                textFormat=config.approvalMail.firstLine+config.nextLine+candidateFirstName+" "+"from"+" "+candidateCompanyName+config.approvalMail.secondLine+config.nextLine+config.approvalMail.thirdLine+config.nextLine+config.approvalMail.fourthLine
-                console.log(textFormat)
-                sendMail(config.adminEmail, subj, textFormat, function (err, data) {
-                    if (err) {
-                        console.log(err)
-                        reject({ code: 400, message: "Database Error", data: {} });
-                        return;
-                    }
-                    console.log('Admin Approval Mail has been sent !!!');
-                });
-            }   
-        
-        }
-        else
-        {
-            makeOffer=0
-            if(_body.userRoleId==1)
-            {
-                adminApproveStatus=0
-                comment=_body.comment
-                value=[_body.candidateId,adminApproveStatus,comment,_body.elowRate,makeOffer]
-                candidateQueries=candidateQuery.candidateSuperAdminApprovalQuery
-            }
-            else if(_body.userRoleId==2)
-            {
-                adminApproveStatus=0;
-                comment=_body.comment;
-                value=[_body.candidateId,adminApproveStatus,comment,makeOffer]
-                candidateQueries=candidateQuery.candidateAdminApprovalQuery
-                subj="Candidate Rejection Mail";
-                textFormat=config.rejectionMail.firstLine+config.nextLine+candidateFirstName+" "+"from"+" "+candidateCompanyName+config.rejectionMail.secondLine+config.nextLine+config.rejectionMail.thirdLine+config.nextLine+config.rejectionMail.fourthLine
-                sendMail(config.adminEmail, subj, textFormat, function (err, data) {
-                    if (err) {
-                        console.log(err)
-                        reject({ code: 400, message: "Database Error", data: {} });
-                        return;
-                    }
-                    console.log('Candidate Rejection Mail has been sent !!!');
-                });
-            }  
+            var firstName = results.rows[0].firstName
+            var companyName = results.rows[0].companyName
+            candidateFirstName = firstName.fontsize(3).bold()
+            candidateCompanyName = companyName.fontsize(3).bold()
+            if (_body.decisionValue == 1) {
+                makeOffer = 1
+                if (_body.userRoleId == 1) {
+                    adminApproveStatus = 1
+                    comment = _body.comment
+                    value = [_body.candidateId, adminApproveStatus, comment, _body.elowRate, makeOffer]
+                    candidateQueries = candidateQuery.candidateSuperAdminApprovalQuery
+                }
+                else if (_body.userRoleId == 2) {
+                    adminApproveStatus = 1;
+                    comment = _body.comment;
+                    value = [_body.candidateId, adminApproveStatus, comment, makeOffer]
+                    candidateQueries = candidateQuery.candidateAdminApprovalQuery
+                    subj = "Candidate Approval Mail";
+                    textFormat = config.approvalMail.firstLine + config.nextLine + candidateFirstName + " " + "from" + " " + candidateCompanyName + config.approvalMail.secondLine + config.nextLine + config.approvalMail.thirdLine + config.nextLine + config.approvalMail.fourthLine
+                    console.log(textFormat)
+                    sendMail(config.adminEmail, subj, textFormat, function (err, data) {
+                        if (err) {
+                            console.log(err)
+                            reject({ code: 400, message: "Database Error", data: {} });
+                            return;
+                        }
+                        console.log('Admin Approval Mail has been sent !!!');
+                    });
+                }
 
-        }
-        const candidateApprovalQuery = {
-            name: 'admin',
-            text:candidateQueries,
-            values:value
-        }
-        database().query(candidateApprovalQuery, (error, results) => {
-            if (error) {
-                console.log(error)
-                reject({ code: 400, message: "Database Error", data: {} });
-                return;
             }
-            resolve({ code: 200, message: "Candidate Clearance Successsfull", data: {} });
+            else {
+                makeOffer = 0
+                if (_body.userRoleId == 1) {
+                    adminApproveStatus = 0
+                    comment = _body.comment
+                    value = [_body.candidateId, adminApproveStatus, comment, _body.elowRate, makeOffer]
+                    candidateQueries = candidateQuery.candidateSuperAdminApprovalQuery
+                }
+                else if (_body.userRoleId == 2) {
+                    adminApproveStatus = 0;
+                    comment = _body.comment;
+                    value = [_body.candidateId, adminApproveStatus, comment, makeOffer]
+                    candidateQueries = candidateQuery.candidateAdminApprovalQuery
+                    subj = "Candidate Rejection Mail";
+                    textFormat = config.rejectionMail.firstLine + config.nextLine + candidateFirstName + " " + "from" + " " + candidateCompanyName + config.rejectionMail.secondLine + config.nextLine + config.rejectionMail.thirdLine + config.nextLine + config.rejectionMail.fourthLine
+                    sendMail(config.adminEmail, subj, textFormat, function (err, data) {
+                        if (err) {
+                            console.log(err)
+                            reject({ code: 400, message: "Database Error", data: {} });
+                            return;
+                        }
+                        console.log('Candidate Rejection Mail has been sent !!!');
+                    });
+                }
 
-            // resolve({ code: 200, message: "Users listed successfully", data: { Users: results.rows } });
+            }
+            const candidateApprovalQuery = {
+                name: 'admin',
+                text: candidateQueries,
+                values: value
+            }
+            database().query(candidateApprovalQuery, (error, results) => {
+                if (error) {
+                    console.log(error)
+                    reject({ code: 400, message: "Database Error", data: {} });
+                    return;
+                }
+                resolve({ code: 200, message: "Candidate Clearance Successsfull", data: {} });
+
+                // resolve({ code: 200, message: "Users listed successfully", data: { Users: results.rows } });
+            })
         })
-    })
 
     })
 }
