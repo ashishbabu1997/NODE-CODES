d35b92b8c425308c40af5889c7c568365d72fca0
Ashish Babu <ashish.b@ellow.ai>
2020-09-15 22:56:33 +0530

    Updated schema for location API

--- backend_service/company/src/locations/schemas/updateLocationSchema.ts
+++ backend_service/company/src/locations/schemas/updateLocationSchema.ts
@@ -1,17 +1,100 @@
 import * as Joi from '@hapi/joi';
 
 export default Joi.object().keys({
-    companyAddress: Joi.string().required().error(errors => {
+    streetAddress1: Joi.string().required().error(errors => {
         errors.forEach(err => {
             switch (err.code) {
                 case "any.required":
-                    err.message = "Company address should not be empty";
+                    err.message = "streetAddress1 should not be empty";
                     break;
                 case "string.base":
-                    err.message = "Company address must be a string"
+                    err.message = "streetAddress1 must be a string"
                     break;
                 default:
-                    err.message = "Invalid Company address"
+                    err.message = "Invalid streetAddress1"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    streetAddress2: Joi.string().required().error(errors => {
+        errors.forEach(err => {
+            switch (err.code) {
+                case "any.required":
+                    err.message = "streetAddress2 should not be empty";
+                    break;
+                case "string.base":
+                    err.message = "streetAddress2 must be a string"
+                    break;
+                default:
+                    err.message = "Invalid streetAddress2"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    city: Joi.string().required().error(errors => {
+        errors.forEach(err => {
+            switch (err.code) {
+                case "any.required":
+                    err.message = "city should not be empty";
+                    break;
+                case "string.base":
+                    err.message = "city must be a string"
+                    break;
+                default:
+                    err.message = "Invalid city"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    zipCode: Joi.number().required().error(errors => {
+        errors.forEach(err => {
+            console.log(err)
+            switch (err.code) {
+                case "any.required":
+                    err.message = "zipCode should not be empty!";
+                    break;
+                case "number.base":
+                    err.message = "zipCode must be a number"
+                    break;
+                default:
+                    err.message = "Invalid zipCode"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    stateId: Joi.number().required().error(errors => {
+        errors.forEach(err => {
+            console.log(err)
+            switch (err.code) {
+                case "any.required":
+                    err.message = "stateId should not be empty!";
+                    break;
+                case "number.base":
+                    err.message = "stateId must be a number"
+                    break;
+                default:
+                    err.message = "Invalid stateId"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    countryId: Joi.number().required().error(errors => {
+        errors.forEach(err => {
+            console.log(err)
+            switch (err.code) {
+                case "any.required":
+                    err.message = "Country Id should not be empty!";
+                    break;
+                case "number.base":
+                    err.message = "Country Id must be a number"
+                    break;
+                default:
+                    err.message = "Invalid Country Id"
                     break;
             }
         });
@@ -34,20 +117,4 @@ export default Joi.object().keys({
         });
         return errors;
     }),
-    countryId: Joi.number().required().error(errors => {
-        errors.forEach(err => {
-            switch (err.code) {
-                case "any.required":
-                    err.message = "Country Id should not be empty!";
-                    break;
-                case "number.base":
-                    err.message = "Country Id must be a number"
-                    break;
-                default:
-                    err.message = "Invalid Country Id"
-                    break;
-            }
-        });
-        return errors;
-    })
 });
