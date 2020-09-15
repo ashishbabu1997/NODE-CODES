c8cb23dbf6fea66f1f0b30b1103269dd8c2cdb0b
Ashish Babu <ashish.b@ellow.ai>
2020-09-15 16:55:05 +0530

    Updated countries and states API

--- backend_service/settings/src/states/statesApi.manager.ts
+++ backend_service/settings/src/states/statesApi.manager.ts
@@ -2,7 +2,7 @@ import config from '../config/config'
 export const listStates = (_body)=> {
     return new Promise((resolve, reject) => {
             const states=config.states
-            const result = states.filter(state => state.countryId =_body.countryId);
+            const result = states.filter(state=>state.country_id ==_body.countryId);
             resolve({ code: 200, message: "States listed Succesfully", data: { states: result } });
     });
 }
