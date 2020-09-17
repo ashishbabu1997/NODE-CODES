ff4184e4c71e30310e0e8b8f7d3d12fcfdbefba2
Ashish Babu <ashish.b@ellow.ai>
2020-09-17 10:16:50 +0530

    Updated jobsrecieved and positions API

---
+++ backend_service/company/src/locations/schemas/updateLocationSchema.ts
@@ -0,0 +1,120 @@
+import * as Joi from '@hapi/joi';
+
+export default Joi.object().keys({
+    streetAddress1: Joi.string().required().error(errors => {
+        errors.forEach(err => {
+            switch (err.code) {
+                case "any.required":
+                    err.message = "streetAddress1 should not be empty";
+                    break;
+                case "string.base":
+                    err.message = "streetAddress1 must be a string"
+                    break;
+                default:
+                    err.message = "Invalid streetAddress1"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    streetAddress2: Joi.string().required().error(errors => {
+        errors.forEach(err => {
+            switch (err.code) {
+                case "any.required":
+                    err.message = "streetAddress2 should not be empty";
+                    break;
+                case "string.base":
+                    err.message = "streetAddress2 must be a string"
+                    break;
+                default:
+                    err.message = "Invalid streetAddress2"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    city: Joi.string().required().error(errors => {
+        errors.forEach(err => {
+            switch (err.code) {
+                case "any.required":
+                    err.message = "city should not be empty";
+                    break;
+                case "string.base":
+                    err.message = "city must be a string"
+                    break;
+                default:
+                    err.message = "Invalid city"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    zipCode: Joi.number().required().error(errors => {
+        errors.forEach(err => {
+            console.log(err)
+            switch (err.code) {
+                case "any.required":
+                    err.message = "zipCode should not be empty!";
+                    break;
+                case "number.base":
+                    err.message = "zipCode must be a number"
+                    break;
+                default:
+                    err.message = "Invalid zipCode"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    stateId: Joi.number().required().error(errors => {
+        errors.forEach(err => {
+            console.log(err)
+            switch (err.code) {
+                case "any.required":
+                    err.message = "stateId should not be empty!";
+                    break;
+                case "number.base":
+                    err.message = "stateId must be a number"
+                    break;
+                default:
+                    err.message = "Invalid stateId"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    countryId: Joi.number().required().error(errors => {
+        errors.forEach(err => {
+            console.log(err)
+            switch (err.code) {
+                case "any.required":
+                    err.message = "Country Id should not be empty!";
+                    break;
+                case "number.base":
+                    err.message = "Country Id must be a number"
+                    break;
+                default:
+                    err.message = "Invalid Country Id"
+                    break;
+            }
+        });
+        return errors;
+    }),
+    locationId: Joi.number().required().error(errors => {
+        errors.forEach(err => {
+            console.log(err)
+            switch (err.code) {
+                case "any.required":
+                    err.message = "Location Id should not be empty!";
+                    break;
+                case "number.base":
+                    err.message = "Location Id must be a number"
+                    break;
+                default:
+                    err.message = "Invalid Location Id"
+                    break;
+            }
+        });
+        return errors;
+    }),
+});
