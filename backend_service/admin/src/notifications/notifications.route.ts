c76a6b0c4250b70a7f348cf138770968cbcfc4ff
Joe Davis <joe.davis@cabotsolutions.com>
2020-09-23 23:10:05 +0530

    notifiaction

---
+++ backend_service/admin/src/notifications/notifications.route.ts
@@ -0,0 +1,6 @@
+import { listNotifications } from './notifications.controller';
+import * as express from 'express';
+const router = express.Router();
+router
+    .get('/',listNotifications)
+export default router;
