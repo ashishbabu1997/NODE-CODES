2bfbe111a979bdc2b65cd85d9c964f7708fe8c05
Joe Davis <joe.davis@cabotsolutions.com>
2020-09-24 14:33:45 +0530

    candidate interview notification

---
+++ backend_service/candidate/src/common/notifications/notifications.ts
@@ -0,0 +1,30 @@
+import notificationsQuery from './notifications.query';
+import database from '../database/database';
+
+
+export const createNotification = async (_body) => {
+    return new Promise((resolve, reject) => {
+        const currentTime = Math.floor(Date.now() / 1000);
+        (async () => {
+            const client = await database().connect()
+            try {
+                await client.query('BEGIN');
+                const changePositionStatusQuery = {
+                    name: 'create-notification',
+                    text: notificationsQuery.createNotification,
+                    values: [_body.positionId, _body.candidateId, _body.message, _body.companyId, _body.notificationType, _body.jobReceivedId, currentTime]
+                }
+                await client.query(changePositionStatusQuery);
+                await client.query('COMMIT');
+                resolve({ code: 200, message: "Notifiaction added successfully", data: {} });
+            } catch (e) {
+                await client.query('ROLLBACK')
+                reject({ code: 400, message: "Failed. Please try again.", data: {} });
+            } finally {
+                client.release();
+            }
+        })().catch(e => {
+            reject({ code: 400, message: "Failed. Please try again.", data: {} })
+        })
+    })
+}
