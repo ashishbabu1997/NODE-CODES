2747cf2c71302f47c407beb6cd1056f072a413bc
Ashish Babu <ashish.b@ellow.ai>
2020-09-25 15:53:32 +0530

    Updated changed password API

--- backend_service/auth/src/changePassword/changePassword.manager.ts
+++ backend_service/auth/src/changePassword/changePassword.manager.ts
@@ -4,27 +4,45 @@ import { Promise } from 'es6-promise'
 import * as crypto from 'crypto'
 export const addNewPassword = (_body) => {
   return new Promise((resolve, reject) => {
-    if (_body.newPassword == _body.confirmPassword)
-    {   
-        var hashedPassword = crypto.createHash("sha256").update(_body.newPassword).digest("hex");
-        const query = {
-            name: 'add-password',
-            text: Query.addPassword,
-            values: [_body.email,hashedPassword],
-          }
-        database().query(query, (error, results) => {
-        if (error) {
-            console.log(error)
-            reject({ code: 400, message: "Error in database connection.", data:{} });
-            return;
+    var passwordHashed = crypto
+                    .createHash("sha256")
+                    .update(_body.currentPassword)
+                    .digest("hex");
+            const query = {
+            name: 'check-password',
+            text: Query.checkPassword,
+            values: [_body.email, passwordHashed],
             }
-        else {
-        resolve({ code: 200, message: "Password changed successfully", data: {} });
-        }
-     })
-    }
-    else{
-        reject({ code: 400, message: "Password does'nt  match", data:{} });
-        }
+            database().query(query, (error, results) => {
+            if (error || results.rows[0]===undefined) {
+                console.log(error)
+                reject({ code: 400, message: "Your current password or email is incorrect", data: {} });
+                return;
+            }
+            if (_body.newPassword == _body.confirmPassword)
+                      {   
+                          var hashedPassword = crypto.createHash("sha256").update(_body.newPassword).digest("hex");
+                            const query = {
+                                name: 'add-password',
+                                text: Query.addPassword,
+                                values: [_body.email,hashedPassword],
+                              }
+                              console.log(hashedPassword)
+                            database().query(query, (error, results) => {
+                            if (error) {
+                                console.log(error)
+                                reject({ code: 400, message: "Error in database connection.", data:{} });
+                                return;
+                                }
+                            else {
+                            resolve({ code: 200, message: "Password changed successfully", data: {} });
+                            }
+                        })
+              }
+              else{
+                          reject({ code: 400, message: "Password does'nt  match", data:{} });
+                  }
+          
+            })
   })
 }
--- backend_service/auth/src/changePassword/query/query.ts
+++ backend_service/auth/src/changePassword/query/query.ts
@@ -1,3 +1,4 @@
 export default {
-    addPassword:`UPDATE employee SET password=$2 WHERE email=$1`
+    addPassword:`UPDATE employee SET password=$2 WHERE email=$1`,
+    checkPassword:'SELECT employee_id FROM employee WHERE email=$1 AND password=$2'
 };
