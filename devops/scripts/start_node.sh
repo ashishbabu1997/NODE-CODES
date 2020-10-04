2e3375612c68de1eea40e4369b84c33d6d82988c
rinishkn <rinishkn@ellow.ai>
2020-10-04 13:12:48 +0530

    pipeline script updates

--- devops/scripts/start_node.sh
+++ devops/scripts/start_node.sh
@@ -23,5 +23,5 @@ cd $NODE_PATH/candidate
 npm run preprod
 sleep 3
 pm2 start admin auth candidate company employee jobs settings 
+ps -ef |grep /DEV/devops/BE/codedeploy/backend_service/ |grep -v grep
 netstat -nlpt |grep :400[0-7]
- ps -ef |grep /DEV/devops/BE/codedeploy/backend_service/ |grep -v grep
