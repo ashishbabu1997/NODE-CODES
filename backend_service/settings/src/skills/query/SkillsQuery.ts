b405d579d033a248f7c34b5cb10bac82ace00a35
Ashish Babu <ashish.b@ellow.ai>
2020-10-16 10:52:42 +0530

    Updated loctions

--- backend_service/settings/src/skills/query/SkillsQuery.ts
+++ backend_service/settings/src/skills/query/SkillsQuery.ts
@@ -4,6 +4,6 @@ export default {
     updateSkills: `update skills set skill_name = $1, job_category_id = $4, updated_on = $2 where skill_id = $3 and status = true`,
     deleteSkills: `update skills set status = false,updated_on = $1 where skill_id = $2`,
     addJobCategorySkills:`Insert into job_category_skills (job_category_id,skill_id,created_on,updated_on) values ($1,$2,$3,$3)`,
-    getSkillsWithoutId:`select s.skill_id as "skillId", s.skill_name as "skillName",s.top_rated as "topRated" from  skills s left join job_category_skills jcs on jcs .skill_id = s.skill_id and jcs.status = true where s.status  = true`,
+    getSkillsWithoutId:`select s.skill_id as "skillId", s.skill_name as "skillName",s.top_rated as "topRated" from  skills s where s.status  = true`,
 }
 
