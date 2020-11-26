a092b37ca8c1558e676b26840c8988e270d5e1e2
Lakshmi Narasimhan R <lakshmi.n@ellow.ai>
2020-11-26 12:09:09 +0530

    redeploy 2

--- backend_service/candidate/src/freelancer/freelancer.manager.ts
+++ backend_service/candidate/src/freelancer/freelancer.manager.ts
@@ -83,7 +83,7 @@ export const modifyOtherInfoAndSubmit = (_body) => {
                 await client.query(queryService.addSkillRelatedTraits(_body));
                 await client.query(queryService.modifySocialProfileAndStatusUpdate(_body));
                 await client.query(queryService.candidateStatusUpdate(_body));
-                await client.query('COMMIT');
+                await client.query('COMMIT'); 
                 
                 resolve({ code: 200, message: "Freelancer other info updated and submitted successfully", data: {} });
             } catch (e) {
