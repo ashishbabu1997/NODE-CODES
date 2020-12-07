18ea830d5924fceb121528628dd3079a913cac30
Ashish Babu <ashish.b@ellow.ai>
2020-12-07 18:19:02 +0530

    Updated notifications get API

--- backend_service/admin/src/notifications/query/notifications.query.ts
+++ backend_service/admin/src/notifications/query/notifications.query.ts
@@ -1,3 +1,3 @@
 export default {
-    listNotifications: `select position_id as "positionId",candidate_id as "candidateId",job_received_id as "jobReceivedId",message,company_id as "companyId",notification_id as "notificationId",notification_type as "notificationType" from notifications where status = true order by notification_id DESC`
+    listNotifications: `select position_id as "positionId",candidate_id as "candidateId",job_received_id as "jobReceivedId",message,company_id as "companyId",notification_id as "notificationId",notification_type as "notificationType",user_role_id as "userRoleId",created_on as "time" from notifications where status = true order by notification_id DESC`
 }
