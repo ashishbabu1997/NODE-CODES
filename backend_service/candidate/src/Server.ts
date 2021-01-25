e92c28cd3ad77c5e985fae4f7c48240edf8e9911
Lakshmi Narasimhan R <lakshmi.n@ellow.ai>
2021-01-25 16:10:38 +0530

    socket trial implementation

--- backend_service/candidate/src/Server.ts
+++ backend_service/candidate/src/Server.ts
@@ -6,6 +6,10 @@ import router from './candidate.router';
 import configurePassport from './config/passportJwtConfig';
 import {swaggerSpec} from './swagger';
 import * as swaggerUi from 'swagger-ui-express';
+import * as socketio from "socket.io";
+import * as path from "path";
+
+
 
 const app = express();
 
@@ -22,4 +26,41 @@ app.use('/', router);
 
 app.listen(AppConfig.http.port, () => {
   console.log('Listening on port ' + AppConfig.http.port);
+});
+
+
+app.set("port", process.env.PORT || 3002);
+
+let http = require("http").Server(app);
+// set up socket.io and bind it to our
+// http server.
+let io = require("socket.io")(http ,{
+  origins: 'http://localhost:*' // I think it should be written like this right? Otherwise there would be syntax error
+});
+
+// whenever a user connects on port 3000 via
+// a websocket, log that a user has connected
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = socket => {
+  const response = new Date();
+  // Emitting a new message. Will be consumed by the client
+  socket.emit("FromAPI", response);
+};
+
+
+const server = http.listen(3002, function() {
+  console.log("listening on *:3000");
 });
