df98de1488f153ea8b984296031e25553018da00
Lakshmi Narasimhan R <lakshmi.n@ellow.ai>
2021-02-01 13:48:37 +0530

    availability sort fix

--- backend_service/candidate/src/utils/utils.ts
+++ backend_service/candidate/src/utils/utils.ts
@@ -127,9 +127,12 @@ export const resourceSort = (body) => {
     if (body.sortBy && body.sortType && Object.keys(orderBy).includes(body.sortBy)) {
         if(body.sortBy=="availability")
         {
-            sort= `order by availability,"readyToStart" ${body.sortType}`;
+            sort= `order by availability desc,"readyToStart" ${body.sortType}`;
         }
-        sort = ` ORDER BY ${orderBy[body.sortBy]} ${body.sortType}`;
+        else
+        {
+            sort = ` ORDER BY ${orderBy[body.sortBy]} ${body.sortType}`;
+        }
 
     }
     return sort;
