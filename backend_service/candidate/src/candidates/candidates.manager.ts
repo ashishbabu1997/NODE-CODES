756baec8ea7e4b721d8c3f35bae8a914bc65da4f
Ashish Babu <ashish.b@ellow.ai>
2021-04-16 10:16:42 +0530

    Rectifying CORS

--- backend_service/candidate/src/candidates/candidates.manager.ts
+++ backend_service/candidate/src/candidates/candidates.manager.ts
@@ -2198,6 +2198,24 @@ export const singleSignOn = (_body) => {
         };
         const result = await request1.get(options);
         console.log(result)
+            // https.get('http://api.linkedin.com/v2/me?oath2_access_token'+content.access_token, res => {
+            // let data = [];
+            // const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+            // console.log('Status Code:', res.statusCode);
+            // console.log('Date in Response header:', headerDate);
+
+            // res.on('data', chunk => {
+            //     data.push(chunk);
+            // });
+
+            // res.on('end', () => {
+            //     console.log('Response ended: ');
+            //     const users = JSON.parse(Buffer.concat(data).toString());
+            //     console.log(users)
+            // });
+            // }).on('error', err => {
+            // console.log('Error: ', err.message);
+            // });
             resolve({ code: 200, message: "Candidate Vetting Updated successfully", data: {} })
             } catch (e) {
                 console.log(e)
