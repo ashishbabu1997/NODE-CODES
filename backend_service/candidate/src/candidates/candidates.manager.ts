53a672ce2af523f06059ec3289ab856120ab4e2f
Lakshmi Narasimhan R <lakshmi.n@ellow.ai>
2021-05-27 11:44:43 +0530

    name update share pdf

--- backend_service/candidate/src/candidates/candidates.manager.ts
+++ backend_service/candidate/src/candidates/candidates.manager.ts
@@ -1868,11 +1868,7 @@ export const createPdfFromHtml = (_body) => {
                 _body.sharedEmails = _body.sharedEmails.filter(elements => elements != null);
 
                 let options = { format: 'A4', printBackground: true, headless: false, args: ['--no-sandbox', '--disable-setuid-sandbox'] };
-                // Example of options with args //
-                // let options = { format: 'A4', args: ['--no-sandbox', '--disable-setuid-sandbox'] };
-                // console.log(`Current directory: ${process.cwd()}`);
-                //  let file = {content: fs.readFileSync('./resume.html', 'utf8')};
-                let file = { url: _body.host + "/sharePdf/" + uniqueId };
+                let file = { url: _body.host + "/sharePdf/" + uniqueId , name : _body.name + ".pdf" };
 
                 await htmlToPdf.generatePdf(file, options).then(pdfBuffer => {
 
