abe935c278b79c1448139b4816c45166ed911619
Ashish Babu <ashish.b@ellow.ai>
2021-05-28 17:38:14 +0530

    Updated listFreeCandidates API

--- backend_service/candidate/src/utils/utils.ts
+++ backend_service/candidate/src/utils/utils.ts
@@ -172,19 +172,19 @@ export const resourceTab = (body) =>{
         vettedQuery='  where chsv."candidateStatus"=3 and chsv."blacklisted"=false '
         break;
         case '1':
-        vettedQuery='  where chsv."candidateStatus"=3 and chsv."candidateVetted"=6 and chsv."blacklisted"=false'
-        break;
-        case '2':
         vettedQuery='  where chsv."candidateStatus"=3 and chsv."blacklisted"=false and (chsv."candidateVetted"!=6 or chsv."candidateVetted" is null)'
         break;
+        case '2':
+        vettedQuery='  where chsv."candidateStatus"=3 and chsv."candidateVetted"=6 and chsv."blacklisted"=false'
+        break;
         case '3':
-        vettedQuery=` where (chsv."candidateStatus" = 4 and chsv."createdBy" = ${body.employeeId}) and chsv."blacklisted"=false`
-        break; 
-        case '4':
         vettedQuery='  where chsv."blacklisted"=true'
         break; 
-        case '5':
+        case '4':
         vettedQuery=`  where chsv."companyId"= (select company_id from company where company_type=2) and  chsv."candidateStatus" in (4,9) and chsv."blacklisted"=false  and chsv."createdBy" != ${body.employeeId}`
+        break; 
+        case '5':
+        vettedQuery=` where (chsv."candidateStatus" = 4 and chsv."createdBy" = ${body.employeeId}) and chsv."blacklisted"=false`
         break; 
         
         default:
