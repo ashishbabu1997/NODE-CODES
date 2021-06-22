0209303004f95cd99a967789a8a138a86a7b862a
Lakshmi Narasimhan R <lakshmi.n@ellow.ai>
2021-06-22 12:20:50 +0530

    primary contact fix

--- backend_service/company/src/employee/query/employee.query.ts
+++ backend_service/company/src/employee/query/employee.query.ts
@@ -4,7 +4,7 @@ export default {
     updateEmployee: "UPDATE employee SET firstname=$2, lastname=$3, role_id=$4, contact_number=$5, updated_on=$6, updated_by=$7 WHERE employee_id = $1",
     deleteEmployee: "UPDATE employee SET status=false WHERE employee_id=$1",
     getEmployeeData: 'SELECT employee_id as "employeeId",firstname as "firstName",lastname as "lastName",email as "email",role_id as "roleId",contact_number as "contactNumber" FROM employee WHERE employee_id = $1 ',
-    getCompanyId: "select company_id from employee where employee_id=1",
+    getCompanyId: "select company_id from employee where employee_id=$1",
     updateActiveState: "update employee set status = (NOT status),updated_on=$2,updated_by=$3 where employee_id=$1",
     updatePrimaryContact: "update employee set primary_email = case when employee_id = $2 then true else false end, updated_on = case when employee_id = $2 then $3 else updated_on end, updated_by = case when employee_id = $2 then $4 else updated_by end where company_id = $1",
 
