1e218016eb5995195123b659b8188f436e229a37
Ashish Babu <ashish.b@ellow.ai>
2021-06-28 14:14:01 +0530

    Updated request for screening API

--- backend_service/candidate/src/candidates/candidates.manager.ts
+++ backend_service/candidate/src/candidates/candidates.manager.ts
@@ -2292,7 +2292,9 @@ export const requestForScreeningManager = (_body) => {
             const client = await database()
             try {
                 await client.query('BEGIN');
+                    console.log(_body.candidateId);
                     let candidateDetails=await client.query(queryService.getCandidateMailDetails(_body))
+                    console.log(candidateDetails)
                     await client.query(queryService.updateRequestForScreening(_body))
                     _body.candidateName=utils.capitalize(candidateDetails.rows[0].candidate_first_name)+' '+utils.capitalize(candidateDetails.rows[0].candidate_last_name)
                     _body.candidateEmail=candidateDetails.rows[0].email_address
