d9a7f4037c41d9bcbcc34ecc773301a900bc14f0
Ashish Babu <ashish.b@ellow.ai>
2021-07-12 17:10:07 +0530

    Issues fixed

--- backend_service/candidate/src/candidates/candidates.manager.ts
+++ backend_service/candidate/src/candidates/candidates.manager.ts
@@ -1348,7 +1348,8 @@ export const createPdfFromHtml = (_body) => {
                 var candidateId = _body.candidateId
                 _body.sharedEmails = _body.sharedEmails.filter(elements => elements != null);
                 let pdf = await builder.pdfBuilder(candidateId, _body.host);
-                await client.query(queryService.saveSharedEmailsForpdf(_body))
+               let s= await client.query(queryService.saveSharedEmailsForpdf(_body))
+               console.log(s.rows)
                 emailService.createPdfFromHtmlEmail(_body, pdf);
                 await client.query('COMMIT')
 
