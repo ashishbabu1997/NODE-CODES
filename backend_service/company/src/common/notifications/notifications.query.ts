7555caa258dd5a854f7b8553c29e1b5422c5642c
Ashish Babu <ashish.b@ellow.ai>
2021-07-21 18:30:19 +0530

    removed job received

--- backend_service/company/src/common/notifications/notifications.query.ts
+++ backend_service/company/src/common/notifications/notifications.query.ts
@@ -1,6 +1,6 @@
 export default {
-    createHirerNotification: `INSERT into hirer_notifications (position_id ,candidate_id, message, company_id, notification_type, job_received_id, created_on, updated_on,user_role_id,created_by ) values ($1, $2, $3, $4,$5, $6, $7, $7,$8,$9)`,
-    createNotification: `INSERT into notifications (position_id ,candidate_id, message, company_id, notification_type, job_received_id, created_on, updated_on,user_role_id,created_by,image,first_name,last_name ) values ($1, $2, $3, $4,$5, $6, $7, $7,$8,$9,$10,$11,$12)`,
+    createHirerNotification: `INSERT into hirer_notifications (position_id ,candidate_id, message, company_id,created_on, updated_on,user_role_id,created_by ) values ($1, $2, $3, $4,$5, $6, $6, $7,$8)`,
+    createNotification: `INSERT into notifications (position_id ,candidate_id, message, company_id, notification_type, created_on, updated_on,user_role_id,created_by,image,first_name,last_name ) values ($1, $2, $3, $4,$5, $5, $6, $7,$8,$9,$10,$11)`,
     createProviderNotification: `INSERT into notifications (candidate_id, message, company_id, notification_type, created_on, updated_on,user_role_id,created_by,image,first_name,last_name ) values ($1, $2, $3, $4,$5, $5, $6, $7,$8,$9,$10)`
 
 } 
