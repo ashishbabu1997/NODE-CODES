e3ef0e6c9e9adc1f47fb763de31dde6975718ac0
Ashish Babu <ashish.b@ellow.ai>
2021-07-21 12:23:23 +0530

    Updated updateProfileDetails API

--- backend_service/candidate/src/candidates/candidates.manager.ts
+++ backend_service/candidate/src/candidates/candidates.manager.ts
@@ -490,6 +490,8 @@ export const modifyResumeData = (_body) => {
     })
 }
 
+
+
 // >>>>>>> FUNC. >>>>>>>
 // >>>>>>>>>>> Function to update the candidate's profile details
 export const modifyProfileDetails = (_body) => {
