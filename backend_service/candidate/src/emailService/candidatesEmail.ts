36b2ef054e8394eb520c49db0f6a7b27ae9fa0b5
Ashish Babu <ashish.b@ellow.ai>
2021-07-26 15:15:12 +0530

    Updated mails

--- backend_service/candidate/src/emailService/candidatesEmail.ts
+++ backend_service/candidate/src/emailService/candidatesEmail.ts
@@ -128,8 +128,8 @@ export const linkCandidateWithPositionEMail = async (_body, client) => {
 
                 let recruiterSignDetails = utils.reccuiterSignatureCheck(recruiterEmail);
                 const { signature } = recruiterSignDetails
-                // element.uniqueId = nanoid();
-                // await client.query(queryService.insertRequestToken(element));
+                element.uniqueId = nanoid();
+                await client.query(queryService.insertRequestToken(element));
                 _body.host='https://dev.ellow.io'
                 let replacements = {
                     'positionName': positionName,
@@ -138,7 +138,7 @@ export const linkCandidateWithPositionEMail = async (_body, client) => {
                     'name': `With regards,\n ${recruiterName}`,
                     'number': signature,
                     'filename': `${element.fileName}.pdf`,
-                    // 'link':_body.host+`/approve-candidate?token=${element.uniqueId}&`,
+                    'link':_body.host+`/approve-candidate?token=${element.uniqueId}&`,
 
                 };
 
