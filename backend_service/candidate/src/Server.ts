0a8dc25c46e8cc4a73ae0163d6e4da13e9a30196
Lakshmi Narasimhan R <lakshmi.n@ellow.ai>
2021-08-10 18:10:03 +0530

    Sentry updates

--- backend_service/candidate/src/Server.ts
+++ backend_service/candidate/src/Server.ts
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable max-len */
 /* eslint-disable no-undef */
 import * as express from 'express';
@@ -17,6 +18,8 @@ import * as swaggerUi from 'swagger-ui-express';
 import * as fileUpload from 'express-fileupload';
 import * as rfs from 'rotating-file-stream'; // version 2.x
 import * as path from 'path'; // version 2.x
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 const app = express();
 // rotate daily
@@ -49,6 +52,44 @@ configurePassport();
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use('/', router);
 app.use(express.static(__dirname + '/public'));
+
+
+Sentry.init({
+  dsn: 'https://04823e79eb4a4e6fb5eef08ac88a5309@o950107.ingest.sentry.io/5898778',
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({tracing: true}),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({app}),
+  ],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
+app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
+
+app.get('/debug-sentry', function mainHandler(req, res) {
+  throw new Error('My first Sentry error!');
+});
+
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + '\n');
+});
+
 
 app.listen(AppConfig.http.port, () => {
   console.log('Listening on port ' + AppConfig.http.port);
