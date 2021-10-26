e0ebd61f9392d2f8d925e8aacfaa766c74256a8c
Lakshmi Narasimhan <lakshmiu.narasimhan@bukuwarung.com>
2021-10-26 18:57:25 +0530

    Sort using ellow rate in incontract * Add a new api for incontract reources

--- backend_service/candidate/src/candidates/candidates.controller.ts
+++ backend_service/candidate/src/candidates/candidates.controller.ts
@@ -25,6 +25,14 @@ export const listFreeCandidates = (req, res) => {
       .catch((error: any) => sendResponse(res, error.code, 0, 400, error.message, error.data));
 };
 
+
+export const listIncontractResources = (req, res) => {
+  const body = req;
+  candidateManager.listIncontractResources(body).then((response: any) => sendResponse(res, response.code, 1, 200, response.message, response.data))
+      .catch((error: any) => sendResponse(res, error.code, 0, 400, error.message, error.data));
+};
+
+
 export const listForAddFromListCandidates = (req, res) => {
   const body = req;
   candidateManager.listAddFromListCandidates(body).then((response: any) => sendResponse(res, response.code, 1, 200, response.message, response.data))
