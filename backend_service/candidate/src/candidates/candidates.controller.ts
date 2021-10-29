094a26c32b667704205b9c11d60d3979c2734cf2
Ashish Babu <ashish.babu@ellow.io>
2021-10-29 14:15:43 +0530

    Updated hiring API

--- backend_service/candidate/src/candidates/candidates.controller.ts
+++ backend_service/candidate/src/candidates/candidates.controller.ts
@@ -330,7 +330,7 @@ export const sigOn = (req, res) => {
 export const googleSignon = (req, res) => {
   const body = req.query;
   candidateManager.googleSignIn(body).then((response: any) => {
-    res.writeHead(301, {Location: 'https://dev.ellow.io/candidate-login?token='+response.data.token});
+    res.writeHead(301, {Location: 'https://dev.ellow.io/candidate-login?googleToken='+response.data.token});
     res.end();
   })
       .catch(() => {
