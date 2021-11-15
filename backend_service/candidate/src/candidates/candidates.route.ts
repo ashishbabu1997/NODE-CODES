b00a152185a5453968863fb215a23f9253557cc0
Ashish Babu <ashish.babu@ellow.io>
2021-11-15 12:41:09 +0530

    Updated referralList API

--- backend_service/candidate/src/candidates/candidates.route.ts
+++ backend_service/candidate/src/candidates/candidates.route.ts
@@ -84,6 +84,6 @@ router
   .post('/checkReferralMail', jwtAuth, setData(), candidateController.checkReferralMailController)
   .get('/candidateReferralList', jwtAuth, setData(), candidateController.candidateReferralListController)
   .get('/candidateAdminReferralList', jwtAuth, setData(), candidateController.candidateAdminReferralListController)
-  .post('/getEmailAddressFromReferralToken', candidateController.getEmailAddressFromReferralTokenController);
+  .get('/getEmailAddressFromReferralToken', candidateController.getEmailAddressFromReferralTokenController);
 
 export default router;
