ed97e59171d7804355657ffe47b76d118bcebffd
Ashish Babu <ashish.babu@ellow.io>
2021-12-27 15:43:35 +0530

    Resolved bugs and created new API for candidate basic details

--- backend_service/candidate/src/candidates/candidate.documentations.ts
+++ backend_service/candidate/src/candidates/candidate.documentations.ts
@@ -2962,6 +2962,8 @@
  *       500:
  *         description: Server down
  */
+
+
 /**
  * @swagger
  * /candidates/getEmailAddressFromReferralToken:
@@ -3031,6 +3033,136 @@
  *       - application/json
  *     produces:
  *       - application/json
+ *     responses:
+ *       200:
+ *         description: Api success
+ *       400:
+ *         description: Api Failed
+ *       401:
+ *         description: Unauthorised access
+ *       403:
+ *         description: Permission denied
+ *       500:
+ *         description: Server down
+ */
+
+
+ /**
+ * @swagger
+ * /candidates/addReport:
+ *   post:
+ *     tags:
+ *       - Candidates
+ *     name:  add reporter 
+ *     summary: Add Reporter Details
+ *     security:
+ *       - bearerAuth: []
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             token:
+ *               type: string
+ *             reporterName:
+ *               type: string
+ *             reporterEmail:
+ *               type: string
+ *              reporterPhoneNumber:
+ *               type: string
+ *           required:
+ *             - token
+ *     responses:
+ *       200:
+ *         description: Api success
+ *       400:
+ *         description: Api Failed
+ *       401:
+ *         description: Unauthorised access
+ *       403:
+ *         description: Permission denied
+ *       500:
+ *         description: Server down
+ */
+
+
+
+  /**
+ * @swagger
+ * /candidates/updateReporterInitialFeedback:
+ *   post:
+ *     tags:
+ *       - Candidates
+ *     name:  add reporter 
+ *     summary: Add Reporter Details
+ *     security:
+ *       - bearerAuth: []
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             comment:
+ *               type: json
+ *             rating:
+ *               type: json
+ *             token:
+ *               type: string
+ *           required:
+ *             - token
+ *     responses:
+ *       200:
+ *         description: Api success
+ *       400:
+ *         description: Api Failed
+ *       401:
+ *         description: Unauthorised access
+ *       403:
+ *         description: Permission denied
+ *       500:
+ *         description: Server down
+ */
+
+
+
+    /**
+ * @swagger
+ * /candidates/updateReporterFinalFeedback:
+ *   post:
+ *     tags:
+ *       - Candidates
+ *     name:  add reporter 
+ *     summary: Add Reporter Details
+ *     security:
+ *       - bearerAuth: []
+ *     consumes:
+ *       - application/json
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - name: body
+ *         in: body
+ *         schema:
+ *           type: object
+ *           properties:
+ *             comment:
+ *               type: json
+ *             rating:
+ *               type: json
+ *             token:
+ *               type: string
+ *           required:
+ *             - token
  *     responses:
  *       200:
  *         description: Api success
