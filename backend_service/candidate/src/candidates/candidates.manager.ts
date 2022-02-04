62978dda85c446e5716fc9f23fb8573909da5726
Ashish Babu <ashish.babu@ellow.io>
2022-02-04 08:41:35 +0530

    Updated sso

--- backend_service/candidate/src/candidates/candidates.manager.ts
+++ backend_service/candidate/src/candidates/candidates.manager.ts
@@ -1849,7 +1849,7 @@ export const singleSignOn = (_body) => {
       try {
         console.log(_body.code)
         const tokenResponse = await fetch(
-          'https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&redirect_uri=https%3A%2F%2Fstagecandidate.ellow.io%2Fapi%2Fv1%2Fcandidates%2FsingleSignOn&client_id=867umqszmeupfh&client_secret=n7oVJe6kbinpdPqu&code=' +
+          'https://www.linkedin.com/oauth/v2/accessToken?grant_type=authorization_code&redirect_uri=https%3A%2F%2Fcandidate.ellow.io%2Fapi%2Fv1%2Fcandidates%2FsingleSignOn&client_id=867umqszmeupfh&client_secret=n7oVJe6kbinpdPqu&code=' +
             _body.code,
           {
             method: 'POST',
